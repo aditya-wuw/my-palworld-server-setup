3c4b6ce727eeab38643aa6e4f28b31ed1d3d89ba
JOhn_Mustard <guddumandal605605@gmail.com>
2026-08-20 16:35:27 +0530

    Fix: get players

--- Bot/src/commands/cmds/players.ts
+++ Bot/src/commands/cmds/players.ts
@@ -14,7 +14,7 @@ interface FilteredPlayersType {
 
 const players = async (interaction: ChatInputCommandInteraction<CacheType>) => {
   const response = await fetch(`${SERVER_API}/v1/getplayers`, {
-    headers: getHeaders("GET", "/v1/getstatus"),
+    headers: getHeaders("GET", "/v1/getplayers"),
   });
   if (!response.ok)
     return await interaction.reply(
